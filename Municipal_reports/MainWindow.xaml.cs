4a44aa4644ad162b6edb16709e740427c9503986
SeanQO <seanquintero3@gmail.com>
2022-01-30 14:42:32 -0500

    added select csv button functionality to load & read csv data and to load the main report that contains all the data points. added letters to the combo box of the report 1, added libraries to create pie chart, created pie chart base

--- Municipal_reports/MainWindow.xaml.cs
+++ Municipal_reports/MainWindow.xaml.cs
@@ -12,6 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+public class Municipality
+{
+
+    public String depCode { get; set; }
+    public String code { get; set; }
+    public String depName { get; set; }
+    public String name { get; set; }
+    public String type { get; set; }
+
+}
 
 namespace Municipal_reports
 {
@@ -20,9 +33,16 @@ namespace Municipal_reports
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<Municipality> municipalities { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            municipalities = new List<Municipality>();
+            Char[] comboBoxItems = new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            comboBoxLetters.ItemsSource = comboBoxItems;
         }
 
         private void dGRep1_SelChanged(object sender, SelectionChangedEventArgs e)
@@ -32,8 +52,36 @@ namespace Municipal_reports
 
         private void loadCsv(object sender, RoutedEventArgs e)
         {
+            String csvPath;
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.DefaultExt = ".csv";
+            openFileDlg.Filter = "Archivos csv (.csv)|*.csv";
+            if (openFileDlg.ShowDialog() == true)
+            {
+                csvPath = openFileDlg.FileName;
+                readCsvFile(csvPath);
+            }
+        }
+
+        private void readCsvFile(String csvPath)
+        {
+            String[] lines = File.ReadAllLines(csvPath);
+            foreach (String line in lines)
+            {
+                String[] data = line.Split(',');
+
+                int temp;
+                if (int.TryParse(data[0], out temp))
+                {
+                    Municipality m = new Municipality() { depCode = data[0], code = data[1], depName = data[2], name = data[3], type = data[4] };
+                    municipalities.Add(m);
+                    dataGridOne.Items.Add(m);
+
+                }
+            }
 
         }
+
         private void letterSelected(object sender, RoutedEventArgs e)
         {
 
